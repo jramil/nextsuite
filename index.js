0f9198ba7ae5c7a95441337f93f9d505831a8c4c
Amilton Pires de Oliveira Júnior <jr_amil@yahoo.com.br>
2020-12-07 00:09:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Principal do Projeto em 06/12/2020 23:43 </h1>
+            <Contador />
+            <div> Filipe Deschamps</div>
+            <div> Como Eu Programo e Hospedo Sites da Forma Mais Moderna que Existe [GUIA DEFINITIVO]</div>
+
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setcontador] = useState(1);
+
+    function adicionarcontador() {
+        setcontador(contador + 1);
+    }
+    return (
+        < div >
+            <div>{contador}</div>
+            <button onClick={adicionarcontador}>Adicionar</button>
+        </div >)
+}
+export default Home
